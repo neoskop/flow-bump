0897b4939e4d74cc7a11654413afb1582cf130f9
draco <wecke@neoskop.de>
2019-08-09 14:17:51 +0200

    chore: update dependencies

--- src/lib/cmd.ts
+++ src/lib/cmd.ts
@@ -1,23 +1,18 @@
+import * as cp from 'child_process';
 import * as execa from 'execa';
 import { Options } from 'execa';
-import { Observable } from 'rxjs/Observable';
-import { Subscriber } from 'rxjs/Subscriber';
-import { filter, first, takeUntil, delay, catchError } from 'rxjs/operators';
-import { fromPromise } from 'rxjs/observable/fromPromise';
-import * as streamToObservable from 'stream-to-observable';
-import * as split from 'split';
-import { merge } from 'rxjs/observable/merge';
-import { concat } from 'rxjs/observable/concat';
-import * as cp from 'child_process';
-import { IScripts } from '../types';
-import { empty } from 'rxjs/observable/empty';
+import { concat, empty, from, merge, Observable, Subscriber } from 'rxjs';
+import { catchError, delay, filter, first, takeUntil } from 'rxjs/operators';
 import { SemVer } from 'semver';
+import * as split from 'split';
+import * as streamToObservable from 'stream-to-observable';
+import { IScripts } from '../types';
 
-export function invokeScript(name : keyof IScripts, { scripts, env = {} } : { scripts?: IScripts, env? : { [key: string]: string } }, ctx? : { version? : SemVer }) : Observable<string> {
-    if(!scripts || !scripts[name]) {
+export function invokeScript(name: keyof IScripts, { scripts, env = {} }: { scripts?: IScripts, env?: { [key: string]: string } }, ctx?: { version?: SemVer }): Observable<string> {
+    if (!scripts || !scripts[name]) {
         return empty();
     }
- 
+
     return runScripts(scripts[name]!, {
         FB_VERSION: ctx && ctx.version ? ctx.version.format() : '',
         FB_VERSION_MAJOR: ctx && ctx.version ? ctx.version.major.toString() : '',
@@ -28,46 +23,45 @@ export function invokeScript(name : keyof IScripts, { scripts, env = {} } : { sc
     })
 }
 
-export function runScripts(scripts : string|string[], env : { [key: string]: string }) : Observable<string> {
-    return Observable.create((subscriber : Subscriber<string>) => {
-        if(!Array.isArray(scripts)) {
-            scripts = [ scripts ];
+export function runScripts(scripts: string | string[], env: { [key: string]: string }): Observable<string> {
+    return Observable.create((subscriber: Subscriber<string>) => {
+        if (!Array.isArray(scripts)) {
+            scripts = [scripts];
         }
         concat(...scripts.map(script => runScript(script, env))).subscribe(subscriber);
     });
 }
 
-export function runScript(script : string, env : { [key: string]: string }) : Observable<string> {
+export function runScript(script: string, env: { [key: string]: string }): Observable<string> {
     return new Observable<string>(subscriber => {
         const child = cp.exec(script, { env: { ...process.env, ...env } }, (err) => {
-            if(err) {
+            if (err) {
                 subscriber.error(err);
             }
             subscriber.complete();
         });
-        
-        streamToObservable(child.stdout.pipe(split())).subscribe(subscriber);
-        streamToObservable(child.stderr.pipe(split())).subscribe(subscriber);
+
+        streamToObservable(child.stdout!.pipe(split())).subscribe(subscriber);
+        streamToObservable(child.stderr!.pipe(split())).subscribe(subscriber);
     })
 }
 
-export function exec(cmd : string, args : string[], options? : Options) : Observable<string> {
-    return Observable.create((subscriber : Subscriber<string>) => {
+export function exec(cmd: string, args: string[], options?: Options): Observable<string> {
+    return new Observable<string>((subscriber) => {
         const child = execa(cmd, args, options);
-        
+
         const promise = Promise.resolve(child);
-        
+
         merge(
-            streamToObservable(child.stdout.pipe(split())),
-            streamToObservable(child.stderr.pipe(split())),
-            fromPromise(promise).pipe(filter(() => false))
+            streamToObservable(child.stdout!.pipe(split())),
+            streamToObservable(child.stderr!.pipe(split())),
+            from(promise).pipe(filter(() => false))
         ).pipe(
-            takeUntil(fromPromise(promise).pipe(delay(1))),
-            filter(Boolean)
+            takeUntil(from(promise).pipe(delay(1))),
         ).subscribe(subscriber);
-        
+
         return () => {
-            if(!child.killed) {
+            if (!child.killed) {
                 child.kill();
             }
         }
@@ -75,65 +69,65 @@ export function exec(cmd : string, args : string[], options? : Options) : Observ
 }
 
 
-export function git(...args : string[]) : Observable<string> {
+export function git(...args: string[]): Observable<string> {
     return exec('git', args);
 }
 
 export module git {
-    export function currentBranch() : Observable<string> {
+    export function currentBranch(): Observable<string> {
         return git('rev-parse', '--abbrev-ref', 'HEAD').pipe(first());
     }
-    
-    export function checkout(branch : string) : Observable<string> {
+
+    export function checkout(branch: string): Observable<string> {
         return git('checkout', branch)
     }
-    
-    export function checkoutOrCreate(branch : string) : Observable<string> {
+
+    export function checkoutOrCreate(branch: string): Observable<string> {
         return checkout(branch).pipe(catchError(() => createBranch(branch)));
     }
-    
-    export function createBranch(name : string, { fromTag, fromCommit } : { fromTag?: string, fromCommit?: string } = {}) : Observable<string> {
-        if(fromTag) {
+
+    export function createBranch(name: string, { fromTag, fromCommit }: { fromTag?: string, fromCommit?: string } = {}): Observable<string> {
+        if (fromTag) {
             return git('checkout', `tags/${fromTag}`, '-b', name);
-        } else if(fromCommit) {
+        } else if (fromCommit) {
             return git('checkout', fromCommit, '-b', name);
         } else {
             return git('checkout', '-b', name);
         }
     }
-    
-    export function removeBranch(name : string, { force } : { force?: boolean } = {}) {
+
+    export function removeBranch(name: string, { force }: { force?: boolean } = {}) {
         return git('branch', force ? '-D' : '-d', name);
     }
-    
+
     export function pull() {
         return git('pull');
     }
-    
-    export function push(remote : string, branch : string, ...options : string[]) {
+
+    export function push(remote: string, branch: string, ...options: string[]) {
         return git('push', '-u', remote, branch, ...options);
     }
-    
-    export function pushTag(remote : string, tag : string) {
+
+    export function pushTag(remote: string, tag: string) {
         return git('push', remote, tag);
     }
-    
-    export function commit(message? : string|null, flags : string[] = []) {
-        if(message) {
+
+    export function commit(message?: string | null, flags: string[] = []) {
+        if (message) {
             return git('commit', '-m', message, ...flags);
         }
         return git('commit', ...flags);
     }
-    
-    export function merge(branch : string, flags : string[] = []) {
+
+    export function merge(branch: string, flags: string[] = []) {
         return git('merge', ...flags, branch);
     }
-    
-    export function tag(tag : string, branch : string) {
+
+    export function tag(tag: string, branch: string) {
         return git('tag', tag, branch);
     }
-    
-    export function fetch(flags : string[] = []) {
+
+    export function fetch(flags: string[] = []) {
         return git('fetch', ...flags);
     }
 }
